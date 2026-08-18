885af4162a11bee19c8c6eb630d8b4b515107a3d
nightfury34631 <mansurmustavi@gmail.com>
2026-08-18 16:29:19 +0600

    Add database configuration template

---
+++ db.example.php
@@ -0,0 +1,12 @@
+<?php
+$conn = mysqli_connect(
+    "localhost",
+    "root",
+    "",
+    "stray_animal_system"
+);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
